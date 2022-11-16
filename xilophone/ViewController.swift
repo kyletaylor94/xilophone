ad5338fb9dd862006f9c5f861a18315f6c54648f
kyletaylor94 <kyletaylorrules94@gmail.com>
2022-11-16 17:58:08 +0100

    final version of xylophone

--- xilophone/ViewController.swift
+++ xilophone/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            playSound(soundName: sender.currentTitle!)
+            
+        }
+        
+        func playSound(soundName: String) {
+            guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+            
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+                
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                
+                guard let player = player else { return }
+                
+                player.play()
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    
+    
+    
 }
 
